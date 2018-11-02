dceb15abb785a548b1de3f1c5b7b8e8ad26efea2
colm.oconnor.github <Colm O'Connor>
2018-11-02 15:31:36 +0000

    Updated hugo.

--- hitch/key.py
+++ hitch/key.py
@@ -6,20 +6,33 @@ import shutil
 
 hugo_dir = DIR.gen / "hugo"
 
+
 def install():
     """
     Set up hugo.
     """
-    if not hugo_dir.exists():
-        Command(
-            "wget", 
-            "https://github.com/gohugoio/hugo/releases/download/v0.31.1/hugo_0.31.1_Linux-64bit.tar.gz",
-        ).in_dir(DIR.gen).run()
+    if hugo_dir.exists():
+        hugo_dir.rmtree(ignore_errors=True)
+    Command(
+        "wget", 
+        "https://github.com/gohugoio/hugo/releases/download/v0.50/hugo_0.50_Linux-64bit.tar.gz",
+    ).in_dir(DIR.gen).run()
 
-        DIR.gen.chdir()
-        patoolib.extract_archive(DIR.gen/"hugo_0.31.1_Linux-64bit.tar.gz")
-        DIR.gen.joinpath("hugo_0.31.1_Linux-64bit.tar/").move(hugo_dir)
-        DIR.gen.joinpath("hugo_0.31.1_Linux-64bit.tar.gz").remove()
+    DIR.gen.chdir()
+    patoolib.extract_archive(DIR.gen/"hugo_0.50_Linux-64bit.tar.gz")
+    DIR.gen.joinpath("hugo_0.50_Linux-64bit.tar").move(hugo_dir)
+    # DIR.gen.joinpath("hugo_0.50_Linux-64bit.tar.gz").remove()
+
+    # Command(
+        # "wget", 
+        # "https://github.com/gohugoio/hugo/releases/download/v0.31.1/hugo_0.31.1_Linux-64bit.tar.gz",
+    # ).in_dir(DIR.gen).run()
+
+    # DIR.gen.chdir()
+    # patoolib.extract_archive(DIR.gen/"hugo_0.31.1_Linux-64bit.tar.gz")
+    # DIR.gen.joinpath("hugo_0.31.1_Linux-64bit.tar/").move(hugo_dir)
+    # DIR.gen.joinpath("hugo_0.31.1_Linux-64bit.tar.gz").remove()
+
 
 
 @expected(CommandError)
